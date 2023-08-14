9c2f12a3b796e7440299ccc625c6fd958da372de
João Victor de Lima <127054365+jvuchoa@users.noreply.github.com>
2023-08-14 20:50:03 -0300

    Add files via upload

---
+++ q2.java
@@ -0,0 +1,12 @@
+public class q2 {
+    public static void main(String[]args) {
+        double a=2;
+        double b=4;
+        if(b>a){
+
+            System.out.println("MAIOR");
+        }else {
+            System.out.println("MENOR");
+        }
+    }
+}
---
+++ q3.java
@@ -0,0 +1,16 @@
+public class q3 {
+
+    public static void main(String[]args) {
+
+        double a=9;
+        double b=10;
+        double c=11;
+
+        double media =(a+b+c)/3;
+        if(media>=7){
+            System.out.println("o aluno foi aprovado com a media: "+media);
+        }else{
+            System.out.println("o aluno foi reprovado com a media de "+media);
+        }
+    }
+}
